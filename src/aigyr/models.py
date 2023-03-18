b894a5ec1c7757439ccae0b36b26383eea40581c
a11sh <alish160606@gmail.com>
2023-03-19 02:20:42 +0600

    models

--- src/aigyr/models.py
+++ src/aigyr/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Attendance(models.Model):
+    date = models.DateField()
+
+class Group(models.Model):
+    name = models.CharField(max_length=30)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone_number = models.CharField(max_length=11)
+    balance = models.IntegerField()
+    email = models.EmailField()
+    password = models.
+    group = models.ForeignKey(Group)
+    attendance = models.ManyToManyField(Attendance)
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
