0c12a1b375f97ad52241d2ae9fe7d62d59cc60a6
Soltan Adil <adil01king@mail.ru>
2023-03-19 14:46:43 +0600

    Ничего особенного, смотрю как модели подключать

--- src/aigyr/models.py
+++ src/aigyr/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+
 class Attendance(models.Model):
     date = models.DateField()
 
+
 class Group(models.Model):
     name = models.CharField(max_length=30)
+
 
 class Student(models.Model):
     first_name = models.CharField(max_length=40)
@@ -12,9 +15,10 @@ class Student(models.Model):
     phone_number = models.CharField(max_length=11)
     balance = models.IntegerField()
     email = models.EmailField()
-    password = models.
-    group = models.ForeignKey(Group)
+    password = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL)
     attendance = models.ManyToManyField(Attendance)
+
 
 class Teacher(models.Model):
     first_name = models.CharField(max_length=40)
